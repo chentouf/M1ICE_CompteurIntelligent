eafd2f9d67bbe06d142abbc2c7a7bc1836588427
chentouf <mychentouf@gmail.com>
2015-11-20 12:13:37 +0100

    Add tests

---
+++ ProjetOpenUp2/src/Tests/ControleurLCDTest.java
@@ -0,0 +1,49 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ControleurLCDTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testControleurLCD() {
+ 
+	}
+
+	@Test
+	public void testGetItsModeleLCD() {
+ 
+	}
+
+	@Test
+	public void testSetItsModeleLCD() {
+ 
+	}
+
+	@Test
+	public void testGetItsVueLCD() {
+ 
+	}
+
+	@Test
+	public void testSetItsVueLCD() {
+ 
+	}
+
+	@Test
+	public void testUpdate() {
+ 
+	}
+
+}
---
+++ ProjetOpenUp2/src/Tests/ControleurLEDTest.java
@@ -0,0 +1,39 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ControleurLEDTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testControleurLED() {
+	}
+
+	@Test
+	public void testGetItsModeleLED() {
+	}
+
+	@Test
+	public void testSetItsModeleLED() {
+	}
+
+	@Test
+	public void testGetItsVueLED() {
+	}
+
+	@Test
+	public void testSetItsVueLED() {
+	}
+
+}
