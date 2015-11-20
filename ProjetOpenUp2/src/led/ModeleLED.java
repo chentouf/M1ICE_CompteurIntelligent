29fa3de49eb645a338af92e330b1645378b2d3c6
yannick-ad <yannick.atchy.dalama@gmail.com>
2015-11-20 11:37:50 +0100

    ajout led
    initial

---
+++ ProjetOpenUp2/src/led/ModeleLED.java
@@ -0,0 +1,38 @@
+/*********************************************************************
+	Rhapsody	: 8.1.3
+	Login		: S219
+	Component	: DefaultComponent
+	Configuration 	: DefaultConfig
+	Model Element	: ModeleLED
+//!	Generated Date	: Fri, 20, Nov 2015 
+	File Path	: DefaultComponent/DefaultConfig/LED/ModeleLED.java
+*********************************************************************/
+
+package led;
+
+import java.util.Observable;
+
+//## class ModeleLED
+
+//----------------------------------------------------------------------------
+// LED/ModeleLED.java                                                                  
+//----------------------------------------------------------------------------
+
+//## package LED 
+
+
+//## class ModeleLED 
+public class ModeleLED extends Observable {
+    
+    
+    // Constructors
+    
+    //## auto_generated 
+    public  ModeleLED() {
+    }
+    
+}
+/*********************************************************************
+	File Path	: DefaultComponent/DefaultConfig/LED/ModeleLED.java
+*********************************************************************/
+
---
+++ ProjetOpenUp2/src/led/VueLED.java
@@ -0,0 +1,45 @@
+/*********************************************************************
+	Rhapsody	: 8.1.3
+	Login		: S219
+	Component	: DefaultComponent
+	Configuration 	: DefaultConfig
+	Model Element	: VueLED
+//!	Generated Date	: Fri, 20, Nov 2015 
+	File Path	: DefaultComponent/DefaultConfig/LED/VueLED.java
+*********************************************************************/
+
+package led;
+
+import java.util.Observable;
+import java.util.Observer;
+
+//## class VueLED 
+
+//----------------------------------------------------------------------------
+// LED/VueLED.java                                                                  
+//----------------------------------------------------------------------------
+
+//## package LED 
+
+
+//## class VueLED 
+public class VueLED implements Observer {
+    
+    
+    // Constructors
+    
+    //## auto_generated 
+    public  VueLED() {
+    }
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+}
+/*********************************************************************
+	File Path	: DefaultComponent/DefaultConfig/LED/VueLED.java
+*********************************************************************/
+
