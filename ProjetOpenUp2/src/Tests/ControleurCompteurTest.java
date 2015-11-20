0fdb5037361bdb366f4f1e3a2e1c0bc663722a1e
chentouf <mychentouf@gmail.com>
2015-11-20 11:49:01 +0100

    ajout test controleur compteur
    inti

---
+++ ProjetOpenUp2/src/Tests/ControleurCompteurTest.java
@@ -0,0 +1,31 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ControleurCompteurTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetConso() {
+	}
+
+	@Test
+	public void testGetItsModeleCompteur() {
+	}
+
+	@Test
+	public void testGetItsVueCompteur() {
+	}
+
+}
