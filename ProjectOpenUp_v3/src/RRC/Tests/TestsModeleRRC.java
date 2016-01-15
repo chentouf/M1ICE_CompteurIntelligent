c0327d5905defce0a393a5b4a35bfca99cd96cbb
sia <sio.coulibaly@gmail.com>
2016-01-15 15:18:40 +0100

    Tests RRC

---
+++ ProjectOpenUp_v3/src/RRC/Tests/TestsModeleRRC.java
@@ -0,0 +1,48 @@
+package RRC.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import RRC.ModeleRRC;
+
+public class TestsModeleRRC {
+
+	ModeleRRC mr;
+	@Before
+	public void setUp() throws Exception {
+		mr = new ModeleRRC(1,2);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		mr = null;
+	}
+
+	@Test
+	public void testModeleRRC() {}
+
+	@Test
+	public void testProduireFacture() {}
+
+	@Test
+	public void testGetPrixEnVigueur() {}
+
+	@Test
+	public void testSetPrixEnVigueur() {}
+
+	@Test
+	public void testGetTabMesuresTraitees() {}
+
+	@Test
+	public void testSetTabMesuresTraitees() {}
+
+	@Test
+	public void testGetItsControleurPasserelle() {}
+
+	@Test
+	public void testSetItsControleurPasserelle() {}
+
+}
---
+++ ProjectOpenUp_v3/src/RRC/Tests/TestsVueRRC.java
@@ -0,0 +1,36 @@
+package RRC.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import RRC.VueRRC;
+
+public class TestsVueRRC {
+
+	VueRRC vr;
+	@Before
+	public void setUp() throws Exception {
+		vr = new VueRRC();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		vr = null;
+	}
+
+	@Test
+	public void testVueRRC() {}
+
+	@Test
+	public void testMajVue() {}
+
+	@Test
+	public void testGetDisplay() {}
+
+	@Test
+	public void testSetDisplay() {}
+
+}
